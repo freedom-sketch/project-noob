727585d10eb4ca4f3f24ecf7c5f8b476cc272ff7
orex2105 <pokomisa55@gmail.com>
2026-01-01 19:57:33 +0300

    refactor: types.go

--- config/type.go
+++ config/type.go
@@ -1,22 +1,8 @@
 package config
 
-var defaultConfig = Config{
-	API:          API{},
-	Panel:        PanelCredentials{},
-	Subscription: SubscriptionMetaData{},
-	Logging:      Logging{},
-}
-
-type PanelCredentials struct {
-	URL      string `json:"url"`
-	Username int    `json:"username"`
-	Password string `json:"password"`
-}
-
 type SubscriptionMetaData struct {
 	ProfileUpdateInterval int    `json:"profile-update-interval"`
 	ProfileTitle          string `json:"profile-title"`
-	SubscriptionUserinfo  string `json:"subscription-userinfo"`
 	SupportUrl            string `json:"support-url"`
 	ProfileWebPageUrl     string `json:"profile-web-page-url"`
 	Announce              string `json:"announce"`
@@ -34,7 +20,6 @@ type Logging struct {
 
 type Config struct {
 	API          API                  `json:"api"`
-	Panel        PanelCredentials     `json:"panel"`
 	Subscription SubscriptionMetaData `json:"subscription"`
 	Logging      Logging              `json:"logging"`
 }
